066f911fc3bf0956608d1f274c748818211bb968
Garrett W <garrettw87@gmail.com>
2018-09-25 12:54:34 -0500

    rearranged funcs and added the rest from spec

--- app/models/package_manager/drupal.rb
+++ app/models/package_manager/drupal.rb
@@ -3,18 +3,8 @@ module PackageManager
     URL = 'https://www.drupal.org'
     COLOR = '#4F5D95'
 
-    def self.package_link(project, version = nil)
-      "https://www.drupal.org/project/#{project.name}" + (version ? "/releases/#{version}" : "")
-    end
-
     def self.project_names
       get_html("https://www.drupal.org/project/project_module/index").css('.view-project-index .view-content li').map{|li| li.css('a').first.try(:text) }
-    end
-
-    def self.recent_names
-      u = 'https://www.drupal.org/project/project_module/feed/all'
-      new_packages = SimpleRSS.parse(get_raw(u)).items.map(&:title)
-      (updated.map { |t| t.split(' ').first } + new_packages).uniq
     end
 
     def self.project(name)
@@ -25,24 +15,65 @@ module PackageManager
     end
 
     def self.mapping(project)
-      return false unless project["versions"].any?
-      # for version comparison of php, we want to reject any dev versions unless
-      # there are only dev versions of the project
-      versions = project["versions"].values.reject {|v| v["version"].include? "dev" }
-      if versions.empty?
-        versions = project["versions"].values
-      end
-      # then we'll use the most recently published as our most recent version
-      latest_version = versions.sort_by { |v| v["time"] }.last
       {
-        :name =>  latest_version['name'],
-        :description => latest_version['description'],
-        :homepage => latest_version['home_page'],
-        :keywords_array => Array.wrap(latest_version['keywords']),
-        :licenses => latest_version['license'].join(','),
-        :repository_url => repo_fallback(project['repository'],latest_version['home_page']),
-        :versions => project["versions"]
+        name: project[:name],
+        keywords_array: Array(project[:page].css('#content div:first a')[1..-1].map(&:text)),
+        description: description(project[:page]),
+        licenses: find_attribute(project[:page], 'License'),
+        homepage: find_attribute(project[:page], 'Home page'),
+        repository_url: repo_fallback(repository_url(find_attribute(project[:page], 'Source repository')), find_attribute(project[:page], 'Home page'))
       }
+    end
+
+    def self.versions(project)
+      versions = find_attribute(project[:page], 'Versions')
+      versions = find_attribute(project[:page], 'Version') if versions.nil?
+      versions.delete('(info)').split(',').map(&:strip).map do |v|
+        {
+          :number => v
+        }
+      end
+    end
+    
+    def self.dependencies(name, version, _project)
+      json = get_json("https://lib.haxe.org/p/#{name}/#{version}/raw-files/haxelib.json")
+      return [] unless json['dependencies']
+      json['dependencies'].map do |dep_name, dep_version|
+        {
+          project_name: dep_name,
+          requirements: dep_version.empty? ? '*' : dep_version,
+          kind: 'runtime',
+          platform: self.name.demodulize
+        }
+      end
+    rescue
+      []
+    end
+
+    def self.recent_names
+      u = 'https://www.drupal.org/project/project_module/feed/all'
+      
+      titles = SimpleRSS.parse(get_raw(u)).items.map(&:title)
+      titles.map { |t| t.split(' ').first }.uniq
+      
+      new_packages = SimpleRSS.parse(get_raw(u)).items.map(&:title)
+      (updated.map { |t| t.split(' ').first } + new_packages).uniq
+    end
+    
+    def self.install_instructions(project, version = nil)
+      "gem install #{project.name}" + (version ? " -v #{version}" : "")
+    end
+
+    def self.package_link(project, version = nil)
+      "https://www.drupal.org/project/#{project.name}" + (version ? "/releases/#{version}" : "")
+    end
+    
+    def self.download_url(name, version = nil)
+      "https://rubygems.org/downloads/#{name}-#{version}.gem"
+    end
+    
+    def self.documentation_url(name, version = nil)
+      "http://www.rubydoc.info/gems/#{name}/#{version}"
     end
   end
 end
